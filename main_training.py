9207891ada0a66afd6aabef669164c11631bce17
georgebakas <georgios.bakas@cern.ch>
2021-03-07 19:26:10 +0200

    The model shows more accuracy when you ignore the first 200 entries (BTC price is flattened compared to latest years). This causes the problem to underfit. By removing 200 entries, the model fits the BTC prices with better accuracy

--- main_training.py
+++ main_training.py
@@ -6,14 +6,16 @@ import datetime as datetime
 import plotly.express as px
 
 import time
-
+import math 
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras import activations
+from sklearn.metrics import accuracy_score
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from tensorflow.keras import optimizers
+from tensorflow.keras.callbacks import EarlyStopping
 from sklearn import preprocessing
 from joblib import dump, load
 
@@ -22,7 +24,7 @@ df = pd.read_csv('data/Binance_BTCUSDT_d.csv', skiprows=[0])
 print(df.dtypes)
 
 print("\n*** DF ***")
-df_copy = df.copy()
+df_copy = df[200:].copy()
 
 def change_timestamp (ts):
     digit_count = len(str(ts))
@@ -39,22 +41,17 @@ df_copy['week_day'] = df_copy.dt.apply(lambda x: x.weekday())
 df_copy.sort_values(by=['unix'],ascending=[True],inplace=True)
 
 df_work = df_copy[['dt','hour','week_day','close','Volume BTC']]
-plt.figure(figsize=(15,7))
-sns.set()
-sns.lineplot(x = df_work.dt, y = 'close',data=df_work).set_title('Offline Rate')
-plt.show()
+#plt.figure(figsize=(15,7))
+#sns.set()
+#sns.lineplot(x = df_work.dt, y = 'close',data=df_work).set_title('Offline Rate')
+#plt.show()
 
 # Normalization
 
 # SPLIT DATA INTO TEST AND TRAIN
 np.random.seed(7)
-
-np.random.seed(7)
-
 X = df_work[['hour','week_day','Volume BTC','close']]
 Y = df_work[['close']]
-#X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, shuffle = False)
-
 
 # NORMALIZATION
 f_transformer = preprocessing.MinMaxScaler((-1,1))
@@ -90,36 +87,79 @@ print("*** SHAPES")
 print(X_train_f.shape, y_train_f.shape)
 print(X_test_f.shape, y_test_f.shape)
 
+early_stopping = EarlyStopping(
+    min_delta=0.001, # minimium amount of change to count as an improvement
+    patience=10, # how many epochs to wait before stopping
+    restore_best_weights=True,
+)
 
 model = keras.Sequential()
 model.add(keras.Input(shape=((X_train_f.shape[1], X_train_f.shape[2]))))
-#model.add(layers.Bidirectional(layers.LSTM(300, activation = 'tanh', return_sequences=False)))
+model.add(layers.Bidirectional(layers.LSTM(128, return_sequences=True)))
+model.add(layers.Bidirectional(layers.LSTM(128, return_sequences=False)))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(64, activation='relu'))
+#model.add(layers.Dense(256, activation='relu'))
+#model.add(layers.LSTM(512, return_sequences=False, activation = 'tanh'))
 #model.add(layers.LSTM(300, return_sequences=False, activation = 'tanh'))
-model.add(layers.LSTM(300, return_sequences=False, activation = 'tanh'))
-model.add(layers.BatchNormalization())
-#model.add(layers.Bidirectional(layers.LSTM(120,activation='relu', return_sequences=True)))
-#model.add(keras.layers.Dropout(rate=0.2))
 #model.add(layers.Flatten())
 #model.add(keras.layers.Dense(units=10, activation = 'relu'))
-model.add(keras.layers.Dense(units=1))
-model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+model.add(layers.Dense(units=1, activation='linear'))
+model.compile(loss='mean_squared_error', optimizer='adam')
 model.summary()
 
 history = model.fit(X_train_f, y_train_f, 
-                batch_size = 200, epochs = 50, 
-                shuffle=False, validation_split=0.2)
+                batch_size = 50, epochs = 50,
+                callbacks=[early_stopping], 
+                shuffle=False, validation_split=0.1)
 
 history_df = pd.DataFrame(history.history)
-# Start the plot at epoch 5
-plt.figure(figsize=(15,7))
-history_df.loc[5:, ['loss', 'val_loss']].plot()
+history_df.loc[:, ['loss', 'val_loss']].plot()
 plt.show()
 
-plt.figure(figsize=(15,7))
-history_df.loc[5:, ['accuracy', 'val_accuracy']].plot()
-plt.show()
 
-print(("Best Validation Loss: {:0.4f}" +\
-    "\nBest Validation Accuracy: {:0.4f}")\
+print(("Best Loss: {:0.4f}" +\
+    "\nBest Validation Loss: {:0.4f}")\
     .format(history_df['loss'].min(), 
-            history_df['val_accuracy'].max()))
+            history_df['val_loss'].min()))
+
+
+y_pred = model.predict(X_test_f) 
+print(y_pred.shape)
+print(y_test_f.shape)
+
+y_test_inv = cnt_transformer.inverse_transform(y_test_f)
+y_pred_inv = cnt_transformer.inverse_transform(y_pred)
+combined_array = np.concatenate((y_test_inv,y_pred_inv),axis=1)
+combined_array2 = np.concatenate((X_test.iloc[time_steps:],combined_array),axis=1)
+print(combined_array)
+
+df_final = pd.DataFrame(data = combined_array, columns=["actual", "predicted"])
+print("size: %d" % (len(combined_array)))
+df_final.head(4)
+
+
+results = model.evaluate(X_test_f, y_test_f)
+
+#print("Accuracy: %s" % (accuracy_score(y_test_inv, y_pred_inv)))
+print("Mean Squared Error: %s" % (mean_squared_error(y_test_inv, y_pred_inv)))
+print(results)
+
+
+# Plots
+
+a = np.repeat(1, len(y_test_inv))
+b = np.repeat(2, len(y_pred_inv))
+
+df1 = pd.DataFrame(data = np.concatenate((y_test_inv,(np.reshape(a, (-1, 1)))),axis=1), columns=["price","type"])
+df2 = pd.DataFrame(data = np.concatenate((y_pred_inv,(np.reshape(b, (-1, 1)))),axis=1), columns=["price","type"])
+
+frames = [df1, df2]
+result = pd.concat(frames, ignore_index=False)
+
+result["type"].replace({1: "actual", 2: "predict"}, inplace=True)
+(result[result.type == "actual"]).head(10)
+
+fig = px.line(result, x=result.index.values, y="price", color='type', title='Bitcoin Price')
+fig.show()
+plt.show()
